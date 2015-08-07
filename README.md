6fd01e1c1b994a6a9efcfffc1662db9bf16516d1
Emanuel Comsa <cremzinc@gmail.com>
2015-08-07 21:37:52 +0300

    Minor adjustments to the code.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Crealytics
 Technical test - Emanuel  Comsa
+
+#Explanation
+The current script reads a CSV with multiple columns. The columns are defined in the class constants. From the fact that it uses a combiner class, I assumed that at any point a bach of csv's need to be merged using a unique column. I think you have multiple CSV types for each customer/campaign and you need to make a single CSV with all the data. Maybe multiple exports from a third party source.
+The script checks a folder for the files that matche the name inputed and then sorts them by the date parsed from the file name.
+Since your code initially had only one file as a param for the combine method from the Combiner class, I assumed that i could work for now with just one CSV source, so i created one for tests.
+You create a hash with the keys being the headers of the CSV files and the values being an array of all the values.
+You now have an enumerator with as many elements as there are lines in the CSV.
+Then, for each of the elements in the enumerator, you output it into the destination CSV, making sure you divide the total rows using the LINES_PER_FILE value.
+You used the combine_values method to change some of the values depending on calculations needed by the business logic.
+
--- app.rb
+++ app.rb
@@ -39,6 +39,8 @@ class Modifier
 
   LINES_PER_FILE = 120000
 
+  DEFAULT_CSV_OPTIONS = { col_sep: "\t", headers: :first_row }
+
   def initialize(saleamount_factor, cancellation_factor)
     @saleamount_factor = saleamount_factor
     @cancellation_factor = cancellation_factor
@@ -52,33 +54,29 @@ class Modifier
     end.combine(input_enumerator)
 
     merger = Enumerator.new do |yielder|
-      while true
-        begin
-          list_of_rows = combiner.next
+      combiner.each do |comb|
+          list_of_rows = comb
           merged = combine_hashes(list_of_rows)
           yielder.yield(combine_values(merged))
-        rescue StopIteration
-          break
-        end
       end
     end
     done = false
     file_index = 0
     file_name = output.gsub('.txt', '')
-    while not done do
-      CSV.open(file_name + "_#{file_index}.txt", "wb", col_sep: "\t", headers: :first_row, row_sep: "\r\n") do |csv|
+    while !done do
+      CSV.open("#{file_name}_#{file_index}.txt", "wb", col_sep: "\t", headers: :first_row, row_sep: "\r\n") do |csv|
         headers_written = false
         line_count = 0
         while line_count < LINES_PER_FILE
           begin
             merged = merger.next
-            if not headers_written
+            unless headers_written
               csv << merged.keys
               headers_written = true
-              line_count +=1
+              line_count += 1
             end
             csv << merged
-            line_count +=1
+            line_count += 1
           rescue StopIteration
             done = true
             break
@@ -95,27 +93,19 @@ class Modifier
     content_as_table = parse(file)
     headers = content_as_table.headers
     index_of_key = headers.index('Clicks')
-    content = content_as_table.sort_by { |a| -a[index_of_key].to_i }
+    content = content_as_table.sort_by { |a| - a[index_of_key].to_i }
     write(content, headers, output)
-    return output
+    output
   end
 
   private
-
-  def combine(merged)
-    result = []
-    merged.each do |_, hash|
-      result << combine_values(hash)
-    end
-    result
-  end
 
   def combine_values(hash)
     LAST_VALUE_WINS.each do |key|
       hash[key] = hash[key].last
     end
     LAST_REAL_VALUE_WINS.each do |key|
-      hash[key] = hash[key].select {|v| !v.nil? && v != 0 && v != '0' && !v.empty?}.last
+      hash[key] = hash[key].reverse.find { |v| !v.nil? && v != 0 && v != '0' && !v.empty? }
     end
     INT_VALUES.each do |key|
       hash[key] = hash[key][0].to_s
@@ -150,8 +140,6 @@ class Modifier
     result
   end
 
-  DEFAULT_CSV_OPTIONS = { :col_sep => "\t", :headers => :first_row }
-
   # Reads the csv file with the default options
   def parse(file)
     CSV.read(file, DEFAULT_CSV_OPTIONS)
@@ -168,15 +156,13 @@ class Modifier
 
   # Writes content and headers to the output csv file
   def write(content, headers, output)
-    CSV.open(output, "wb", { :col_sep => "\t", :headers => :first_row, :row_sep => "\r\n" }) do |csv|
+    CSV.open(output, 'wb', col_sep: "\t", headers: :first_row, row_sep: "\r\n") do |csv|
       csv << headers
       content.each do |row|
         csv << row
       end
     end
   end
-
-
 end
 
 modified = input = latest('test')
@@ -186,4 +172,4 @@ puts "Input: #{input}"
 modifier = Modifier.new(modification_factor, cancellaction_factor)
 modifier.modify(modified, input)
 
-puts "DONE modifying"
+puts 'DONE modifying'
